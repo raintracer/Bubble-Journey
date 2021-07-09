bf7a95c427b3701fcae4f462bd884fdbb7e7175b
Richard Stevenson <rts@rtstevenson.com>
2021-07-09 13:33:00 -0400

    Basic States and Movement Implemented

---
+++ Assets/Input/PlayerInput.cs
@@ -0,0 +1,355 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""f1fb5810-0777-405d-af2b-e2c3e4cc9830"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""84d46526-fd6f-4663-8b44-9b878f0481a5"",
+                    ""expectedControlType"": ""Dpad"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""c0970334-9670-4a52-9658-0da5ff781f96"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""45f51e83-0822-439e-82d1-6ae6f320e66e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Gamepad DPad"",
+                    ""id"": ""26a33a54-1a8f-447a-a226-577e54886e0b"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2e6df17b-618a-4a69-9082-c4b7448363c5"",
+                    ""path"": ""<Gamepad>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""d96f9557-f67e-4136-be88-57f2694412ec"",
+                    ""path"": ""<Gamepad>/dpad/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""b988fd3a-c41b-43be-ae3a-751dc8a5138f"",
+                    ""path"": ""<Gamepad>/dpad/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""4a7e4e73-9f42-457f-b7e4-c25a73b34b6a"",
+                    ""path"": ""<Gamepad>/dpad/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrow Keys"",
+                    ""id"": ""b870b3dc-bcb3-492f-a6c9-49cf8651fa71"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""6e1fc81d-4add-4d1a-b7bf-2685134000d9"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""9681beee-9c88-4113-afc4-3eff8b1d9969"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""9fd43a3d-41b3-44f3-a1a4-51990df7ebd1"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c59dae47-5210-42da-a33e-034c63d22c05"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""4417bc4e-5efc-48d5-a40c-6eb680f0c695"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2bd08ceb-87f1-444e-b31a-e349f7b2d173"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""50990ffd-f4e7-4f14-8997-1baddd520e0b"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""39f728a9-1968-4306-995d-a394c593d305"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8420cd92-f784-419e-a69c-7be487a714ea"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b22112bb-02a2-4872-888d-586139660c33"",
+                    ""path"": ""<Keyboard>/z"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a16bc1ea-9f68-4b37-a260-9fcd45bbdb21"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a522383a-822a-4e3c-a43d-299747f909d4"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Dash;
+    public struct PlayerActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    PlayerInput Inputs;
+    Vector2 Movement = Vector2.zero;
+    GameObject GO;
+    Rigidbody2D RB;
+    SpriteRenderer SR;
+    Animator ANIM;
+    PlayerState State;
+
+    public enum PlayerState { Idle, Run, Jumping, Falling, Dashing }
+
+    const float Speed = 10f;
+    const float JumpSpeed = 20f;
+    const float DashPulseDelay = 0.25f;
+    const float DashStunDelay = 0.25f;
+    bool _JumpInputFlag = false;
+    bool _DashInputFlag = false;
+    bool _OnGroundFlag = false;
+
+    void Awake()
+    {
+        
+        // Capture Object Components
+        GO = GameObject.Find("PlayerObject");
+        ANIM = GO.GetComponent<Animator>();
+        RB = GO.GetComponent<Rigidbody2D>();
+        SR = GO.GetComponent<SpriteRenderer>();
+
+        // Initialize Inputs
+        Inputs = new PlayerInput();
+        Inputs.Enable();
+        Inputs.Player.Move.performed += ctx => Movement = ctx.ReadValue<Vector2>();
+        Inputs.Player.Jump.performed += ctx => _JumpInputFlag = true;
+        Inputs.Player.Dash.performed += ctx => _DashInputFlag = true;
+
+        // Set starting state
+        ChangeState(PlayerState.Idle);
+    }
+
+
+
+    void FixedUpdate()
+    {
+
+        if (_OnGroundFlag)
+        {
+            _OnGroundFlag = false;
+            if (RequestLand()) ChangeState(PlayerState.Idle);
+        }
+
+        if (_JumpInputFlag)
+        {
+            _JumpInputFlag = false;
+            if (RequestJump()) ChangeState(PlayerState.Jumping);
+        }
+        
+        if (_DashInputFlag)
+        {
+            _DashInputFlag = false;
+            if(RequestDash()) ChangeState(PlayerState.Dashing);
+        }
+
+        if (Movement.x != 0)
+        {
+            RequestMove();
+        }
+        else
+        {
+            if (State == PlayerState.Run) ChangeState(PlayerState.Idle);
+        }
+
+        if (State == PlayerState.Jumping)
+        {
+            if (RB.velocity.y < 0) ChangeState(PlayerState.Falling);
+        }
+
+    }
+
+    #region HandleInputs
+    private bool RequestJump()
+    {
+        switch (State)
+        {
+            case PlayerState.Idle:
+            case PlayerState.Run:
+                Jump();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private bool RequestDash()
+    {
+        switch (State)
+        {
+            case PlayerState.Idle:
+            case PlayerState.Run:
+            case PlayerState.Jumping:
+            case PlayerState.Falling:
+                // Dash();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private bool RequestLand()
+    {
+        switch (State)
+        {
+            case PlayerState.Idle:
+            case PlayerState.Run:
+            case PlayerState.Jumping:
+            case PlayerState.Falling:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private bool RequestMove()
+    {
+        switch (State)
+        {
+            case PlayerState.Dashing:
+                return false;
+            default:
+                Move();
+                return true;
+        }
+    }
+
+    #endregion
+
+
+    void SetXVelocityWithForce(float DesiredVelocity)
+    {
+        RB.AddForce((new Vector2(DesiredVelocity, 0) - new Vector2(RB.velocity.x, 0)) / Time.fixedDeltaTime);
+        // ANIM.SetFloat("AbsTargetSpeedX", Mathf.Abs(DesiredVelocity));
+    }
+
+    void SetYVelocityWithForce(float DesiredVelocity)
+    {
+        RB.AddForce((new Vector2(0, DesiredVelocity) - new Vector2(0, RB.velocity.y)) / Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        SetYVelocityWithForce(JumpSpeed);
+        // ANIM.SetTrigger("JumpStartTrigger");
+    }
+
+    void Move()
+    {
+        if (State == PlayerState.Idle)
+        {
+            ChangeState(PlayerState.Run);
+        }
+        
+        if (Movement.x > 0)
+        {
+            SetXVelocityWithForce(Speed);
+            SR.flipX = false;
+        }
+        else if (Movement.x < 0)
+        {
+            SetXVelocityWithForce(-Speed);
+            SR.flipX = true;
+        }
+    }
+
+    private void ChangeState(PlayerState _State)
+    {
+        State = _State;
+        Color[] Colors = { Color.cyan, Color.blue, Color.green, Color.red, Color.yellow};
+        SR.color = Colors[(int)State];
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _OnGroundFlag = true;
+    }
+
+}
